59911e06f450fc67f5264915194d9f3c845b61c3
Thiago Granata <granatathiago@gmail.com>
2023-07-28 10:31:43 -0300

    feat: base skeleton done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+import styles from './App.module.css'
+
+const API_URL = 'https://api.npoint.io/97d89162575a9d816661';
+
+const LOOKING_CURRENCY = ['u$s', '$']
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const getAccountsData = async() => {
+    const response = await fetch(API_URL, {
+      method: 'GET'
+    })
+
+    const data = await response.json();
+
+    console.log(data)
+  }
+
+  useEffect(() => {
+
+    getAccountsData();
+
+  }, [])
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className={styles.heading}>
+        <h1>NCR Challenge</h1>
+      </header>
+      <main className={styles.resume}>
+        <div className={styles.resumeHeading}>
+          <p style={{'fontSize': '1.5rem'}}>Consulta de Saldo</p>
+          <p style={{fontWeight: 'bold'}}>Seleccione la Cuenta a Consultar</p>
+        </div>
+
+        <div className={styles.accountsList}>
+          <button className={styles.account}>
+            <p>Cuenta Corriente</p>
+            <p>Nro: 872378326704</p>
+          </button>
+
+          <button className={styles.account}>
+            <p>Cuenta Corriente</p>
+            <p>Nro: 872378326704</p>
+          </button>
+
+          <button className={styles.account}>
+            <p>Cuenta Corriente</p>
+            <p>Nro: 872378326704</p>
+          </button>
+
+          <button className={styles.account}>
+            <p>Cuenta Corriente</p>
+            <p>Nro: 872378326704</p>
+          </button>
+
+          <button className={styles.account}>
+            <p>Cuenta Corriente</p>
+            <p>Nro: 872378326704</p>
+          </button>
+
+          <button className={styles.account}>
+            <p>Más opciones &#187;</p>
+          </button>
+        </div>
+
+        <hr />
+
+        <footer className={styles.footer}>
+          <button>Salir</button>
+        </footer>
+
+      </main>
     </>
   )
 }
